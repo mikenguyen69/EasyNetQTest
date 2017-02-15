debd8a2694c66b33457ec8a53edc5399217b3127
Nguyen Duc Hung <nguyenduchung0609@gmail.com>
2017-02-15 16:07:06 +0800

    https://github.com/mikenguyen69/EasyNetQTest

---
+++ Subscriber/Program.cs
@@ -0,0 +1,31 @@
+﻿using EasyNetQ;
+using Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Subscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var bus = RabbitHutch.CreateBus("host=localhost"))
+            {
+                bus.Subscribe<TextMessage>("Test", HandleTextMessage);
+
+                Console.WriteLine("Listening for message. Hit <return> to quit.");
+                Console.ReadLine();
+            }
+        }
+
+        static void HandleTextMessage(TextMessage message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Got message: {0}", message.Text);
+            Console.ResetColor();
+        }
+    }
+}
